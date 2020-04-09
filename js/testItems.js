c82ba32596b41200d1313ea65e2964dc6a3bc7b4
Fa-b <41373206+Fa-b@users.noreply.github.com>
2020-04-09 15:43:32 +0200

    fixed image data for dummy items

--- js/testItems.js
+++ js/testItems.js
@@ -1,113 +1,100 @@
 var Items = {
     "testItem1": { // Somewhat regular item
-        "itemColor":14,
-        "image":"mgl",
+        "image":"{\"code\":\"mgl\",\"color\":14,\"sockets\":[]}",
         "title":"Trang-Oul's Claws Heavy Bracers",
         "description":"\xffc2Trang-Oul's Claws (99)\n\xffc2Heavy Bracers\n\xffc0Defense: \xffc374\n\xffc0Durability: 16 of 16\n\xffc0Required Strength: 58\n\xffc0Required Level: 45\n\xffc3+2 to Curses (Necromancer Only)\n\xffc3+20% Faster Cast Rate\n\xffc3+25% to Poison Skill Damage\n\xffc3+30 Defense\n\xffc3Regenerate Mana 255%\n\xffc3Cold Resist +30%\n\xffc4\n\xffc4Trang-Oul's Avatar\n\xffc1Trang-Oul's Girth\n\xffc2Trang-Oul's Claws\n\xffc1Trang-Oul's Wing\n\xffc1Trang-Oul's Scales\n\xffc2\xffc1Trang-Oul's Guise$15:382:7:0:6",
         "header":"header stuff",
-        "sockets":[]
+        "account":"Account",
+        "character":"Character"
     },
     "testItem2": { // Purple item for color testing
-        "itemColor":18,
-        "image":"gth",
+        "image":"{\"code\":\"gth\",\"color\":18,\"sockets\":[\"jew5\"]}",
         "title":"Tal Rasha's Guardianship Lacquered Plate",
         "description":"\xffc2Tal Rasha's Guardianship (90)\n\xffc2Lacquered Plate\n\xffc0Defense: \xffc3919\n\xffc0Durability: 55 of 55\n\xffc1Required Strength: 84\n\xffc1Required Level: 71\n\xffc3+2 to All Skills\n\xffc3+25% Faster Run/Walk\n\xffc3+20% Faster Hit Recovery\n\xffc3+400 Defense\n\xffc3All Resistances +40\n\xffc3Damage Reduced by 25%\n\xffc3Magic Damage Reduced by 15\n\xffc3Requirements -60%\n\xffc3Socketed (1)\n\xffc4\n\xffc4Tal Rasha's Wrappings\n\xffc1Tal Rasha's Horadric Crest\n\xffc2Tal Rasha's Guardianship\n\xffc1Tal Rasha's Lidless Eye\n\xffc1Tal Rasha's Adjudication\n\xffc2\xffc1Tal Rasha's Fine-Spun Cloth$18:440:7:4:0",
         "header":"header stuff",
-        "sockets":["jew5"]
+        "account":"Account",
+        "character":"Character"
     },
     "testItem3": { //Ethereal item with stuff socketed in it
-        "itemColor":-1,
-        "image":"ltp",
+        "image":"{\"code\":\"ltp\",\"color\":-1,\"sockets\":[\"r01\",\"r28\",\"r14\",\"r12\"]}",
         "title":"Fortitude Archon Plate",
         "description":"\xffc4Fortitude (90)\n\xffc5Archon Plate\n\xffc4'ElSolDolLo'\n\xffc0Defense: \xffc32490\n\xffc0Durability: 60 of 69\n\xffc1Required Strength: 93\n\xffc1Required Level: 63\n\xffc320% Chance to cast level 15 Chilling Armor when struck\n\xffc3+25% Faster Cast Rate\n\xffc3+300% Enhanced Damage\n\xffc3+215% Enhanced Defense\n\xffc3+15 Defense\n\xffc3+198 to Life (Based on Character Level)\n\xffc3Replenish Life +7\n\xffc3+5% to Maximum Lightning Resist\n\xffc3All Resistances +30\n\xffc3Damage Reduced by 7\n\xffc312% Damage Taken Goes To Mana\n\xffc3+1 to Light Radius\n\xffc3Increase Maximum Durability 15%\n\xffc3Ethereal (Cannot be Repaired), Socketed (4)$76:443:3:2:0:eth",
         "header":"header stuff",
-        "sockets":[
-            "r01",
-            "r28",
-            "r14",
-            "r12"
-        ]
+        "account":"Account",
+        "character":"Character"
     },
     "testItem4": { //Torch
-        "itemColor":-1,
-        "image":"invtrch",
+        "image":"{\"code\":\"invtrch\",\"color\":-1,\"sockets\":[]}",
         "title":"Hellfire Torch",
         "description":"\xffc4Hellfire Torch\n\xffc4Large Charm\n\xffc0Keep in Inventory to Gain Bonus\n\xffc1Required Level: 75\n\xffc35% Chance to cast level 10 Firestorm on striking\n\xffc3+3 to Amazon Skill Levels\n\xffc3+20 to all Attributes\n\xffc3All Resistances +12\n\xffc3+8 to Light Radius\n\xffc3Level 30 Hydra (10/10 Charges)$1007589987:604:7:5:4",
         "header":"header stuff",
-        "sockets":[]
+        "account":"Account",
+        "character":"Character"
     },
     "testItem5": { //Unid Anni
-        "itemColor":-1,
-        "image":"ch1",
+        "image":"{\"code\":\"ch1\",\"color\":-1,\"sockets\":[]}",
         "title":"Small Charm",
         "description":"\xffc4Small Charm\n\xffc0Keep in Inventory to Gain Bonus\n\xffc1Unidentified$104546287:603:3:4:1",
         "header":"header stuff",
-        "sockets":[]
+        "account":"Account",
+        "character":"Character"
     },
     "testItem6": { //Unid Anni
-        "itemColor":-1,
-        "image":"ch1",
+        "image":"{\"code\":\"ch1\",\"color\":-1,\"sockets\":[]}",
         "title":"Small Charm",
         "description":"\xffc4Small Charm\n\xffc0Keep in Inventory to Gain Bonus\n\xffc1Unidentified$104546287:603:3:4:1",
         "header":"header stuff",
-        "sockets":[]
+        "account":"Account",
+        "character":"Character"
     },
     "testItem9": { //Unid Torch
-        "itemColor":-1,
-        "image":"cm22",
+        "image":"{\"code\":\"cm22\",\"color\":-1,\"sockets\":[]}",
         "title":"Large Charm",
         "description":"\xffc4Large Charm\n\xffc0Keep in Inventory to Gain Bonus\n\xffc1Unidentified$1007589987:604:7:5:4",
         "header":"header stuff",
-        "sockets":[]
+        "account":"Account",
+        "character":"Character"
     },
     "testItem10": { //Eth Monarch
-        "itemColor":-1,
-        "image":"kit",
+        "image":"{\"code\":\"kit\",\"color\":-1,\"sockets\":[]}",
         "title":"Monarch",
         "description":"\xffc5Monarch\n\xffc0Defense: 222\n\xffc0\xffc0Chance to Block: \xffc347%\n\xffc0Durability: 44 of 44\n\xffc1Required Strength: 146\n\xffc1Required Level: 54\n\xffc3Ethereal (Cannot be Repaired)$3755160313:447:7:4:5:eth",
         "header":"header stuff",
-        "sockets":[]
+        "account":"Account",
+        "character":"Character"
     },
     "testItem11": { //Eth CV
-        "itemColor":-1,
-        "image":"vou",
+        "image":"{\"code\":\"vou\",\"color\":-1,\"sockets\":[]}",
         "title":"Colossus Voulge",
         "description":"\xffc5Colossus Voulge\n\xffc0Two-Hand Damage: 25 to 247\n\xffc0Durability: 26 of 26\n\xffc1Required Dexterity: 45\n\xffc1Required Strength: 200\n\xffc1Required Level: 48\n\xffc0Polearm Class - Slow Attack Speed\n\xffc3Ethereal (Cannot be Repaired)$1073362527:254:7:0:3:eth",
         "header":"header stuff",
-        "sockets":[]
+        "account":"Account",
+        "character":"Character"
     },
     "testItem12": {
-        "itemColor":-1,
-        "image":"stu",
+        "image":"{\"code\":\"stu\",\"color\":-1,\"sockets\":[\"gemsocket\",\"jew1\",\"gemsocket\"]}",
         "title":"Superior Wire Fleece",
         "description":"\xffc5Superior Wire Fleece\n\xffc0Defense: \xffc3554\n\xffc0Durability: 18 of 32\n\xffc1Required Strength: 111\n\xffc1Required Level: 53\n\xffc3+15% Enhanced Defense\n\xffc3Socketed (4)$4227802827:432:7:2:0",
         "header":"header stuff",
-        "sockets":[
-            "gemsocket",
-            "jew1",
-            "gemsocket"
-        ]
+        "account":"Account",
+        "character":"Character"
     },
 
     "testItem13": {
-        "itemColor": 18,
-        "image":"msk",
+        "image":"{\"code\":\"msk\",\"color\":18,\"sockets\":[\"gemsocket\"]}",
         "title":"Tal Rasha's Horadric Crest Death Mask",
         "description":"\xffc2Tal Rasha's Horadric Crest (99)\n\xffc2Death Mask\n\xffc0Defense: \xffc3131\n\xffc0Durability: 20 of 20\n\xffc0Required Strength: 55\n\xffc0Required Level: 66\n\xffc310% Mana stolen per hit\n\xffc310% Life stolen per hit\n\xffc3+45 Defense\n\xffc3+60 to Life\n\xffc3+30 to Mana\n\xffc3All Resistances +15\n\xffc3Socketed (1)\n\xffc4\n\xffc4Tal Rasha's Wrappings\n\xffc2Tal Rasha's Horadric Crest\n\xffc2Tal Rasha's Guardianship\n\xffc1Tal Rasha's Lidless Eye\n\xffc1Tal Rasha's Adjudication\n\xffc2\xffc1Tal Rasha's Fine-Spun Cloth$52:358:7:4:0",
         "header":"Header",
-        "sockets":["gemsocket"]
+        "account":"Account",
+        "character":"Character"
     },
 	"testItem14": {
-		"itemColor":-1,
-		"image":"pa5",
+		"image":"{\"code\":\"pa5\",\"color\":-1,\"sockets\":[\"gemsocket\",\"gemsocket\",\"gemsocket\"]}",
 		"title":"Superior Vortex Shield",
 		"description":"\xffc5Superior Vortex Shield\n\xffc0Defense: \xffc3250\n\xffc0\xffc0Chance to Block: \xffc344%\n\xffc0Durability: 82 of 90\n\xffc1(Paladin Only)\n\xffc1Required Strength: 148\n\xffc1Required Level: 66\n\xffc3+11% Enhanced Defense\n\xffc3All Resistances +45\n\xffc3Socketed (3)$3667503528:502:3:0:0",
 		"header":"header",
-		"sockets":[
-			"gemsocket",
-			"gemsocket",
-			"gemsocket"
-		]
+        "account":"Account",
+        "character":"Character"
 	}
 
 
